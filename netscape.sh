7ff5d518239eaf25385f6edc7274235ea5b25bf1
Sebastian Zagrodzki <sebek@zagrodzki.net>
2000-07-26 08:27:11 +0000

    - simplified - modified paths to match FHS2.1
    Changed files:
        netscape.sh -> 1.2
        netscape4.sh -> 1.2

--- netscape.sh
+++ netscape.sh
@@ -1,48 +1,29 @@
 #!/bin/sh
 
-which=""
-
-if echo $0 | grep 'navigator' >/dev/null; then
-    which=netscape-navigator
-elif echo $0 | grep 'communicator' >/dev/null ; then
-    which=netscape-communicator
-fi
-
-if [ -z $which ]; then
-    if rpm -q netscape-communicator >/dev/null 2>&1; then
+PREFIX=/usr/X11R6/bin
+if [ -x "$PREFIX/netscape-communicator" ]; then
 	which=netscape-communicator
-    elif rpm -q netscape-navigator >/dev/null 2>&1; then
+else if [ -x "$PREFIX/netscape-navigator" ]; then
 	which=netscape-navigator
-    else
-	echo "You don't have netscape installed." >&2
-    fi
-fi
-	
-if ! rpm -q $which >/dev/null 2>&1; then
-    echo "You don't have $which installed." >&2
-    exit 1
+else
+	echo "Could not find neither netscape-communicator or navigator!"
+	exit 1
 fi
 
-I="`rpm -q $which --qf '%{INSTALLPREFIX}\n'`"
-if [ "$I" = "(none)" ]; then
-    I=/usr
-fi
-
-
-if [ -x $I/lib/netscape/$which ]; then
+if [ -x $PREFIX/$which ]; then
 
     if [ -z "$*" ]; then
-	HOMEPAGE=/usr/doc/HTML/index.html
+	HOMEPAGE=http://www.pld.org.pl
 	if [ -f $HOME/.netscape/preferences.js ]; then
 	    if grep "browser.startup.homepage" \
 			$HOME/.netscape/preferences.js > /dev/null; then
 		HOMEPAGE=""
 	    fi
 	fi
-	exec $I/lib/netscape/$which $HOMEPAGE
+	exec $PREFIX/$which $HOMEPAGE
     else
-	exec $I/lib/netscape/$which $*
+	exec $PREFIX/$which $*
     fi
 fi
 
-echo "An error occurred running and $I/lib/netscape/$which."
+echo "An error occurred running $PREFIX/$which."
